6accfd28bf279497ecbe77b75c32ccc83675330f
ChilliMads <55391223+ChilliMads@users.noreply.github.com>
2020-10-13 11:28:19 +0200

    Add project files.

---
+++ Lektion 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LanguageBaciscs
+{
+    class Prorgram
+
+    {
+        static void swap(ref int x, ref int y)
+        {
+            int tmp = x;
+            x = y;
+            y = tmp;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var a = 5;
+            var b = 6;
+
+            swap(ref a, ref b);
+            Console.WriteLine($"a={a} and b = {b}");
+        }
+    }
+}
